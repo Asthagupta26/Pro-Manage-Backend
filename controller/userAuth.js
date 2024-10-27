f4290814aead5d012620fdd395f5be5ada295832
Asthagupta26 <guptaastha977@gmail.com>
2024-10-27 15:56:40 +0530

    task issue resolved

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -1,18 +1,18 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Task = require("../models/task");
 
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-   if (!name || !email || !password) {
-      return res.status(400).json({ message: 'Please fill all the fields' });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
     }
-   const existingUser = await User.findOne({ email });
-   if (existingUser) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
       return res.status(400).json({
-        message: 'User already exists',
+        message: "User already exists",
         existingUser: true,
       });
     }
@@ -28,13 +28,13 @@ const registerUser = async (req, res) => {
     await user.save();
 
     res.status(201).json({
-      message: 'User registered successfully',
+      message: "User registered successfully",
       name: user.name,
       email: user.email,
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
@@ -43,47 +43,45 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'Please fill all the fields' });
+      return res.status(400).json({ message: "Please fill all the fields" });
     }
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ message: 'User does not exist' });
+      return res.status(400).json({ message: "User does not exist" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
-    
 
     res.status(200).json({
-      message: 'Login successfull',
+      message: "Login successfull",
       token,
       email: user.email,
       name: user.name,
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
 const updateUserName = async (req, res, next) => {
-
   try {
-    const email = req.query.email || '';
-    const name = req.query.name || '';
+    const email = req.query.email || "";
+    const name = req.query.name || "";
 
     if (!email || !name) {
       return res.status(400).json({
-        message: 'Bad Request',
+        message: "Bad Request",
       });
     }
 
@@ -93,24 +91,23 @@ const updateUserName = async (req, res, next) => {
 
     if (!userDetails) {
       return res.status(400).json({
-        message: 'Bad request',
+        message: "Bad request",
       });
     }
 
     await User.updateOne(
       { email: email },
-  
+
       {
-        $set: {   
+        $set: {
           name: name,
           email: userDetails?.email,
           password: userDetails?.password,
-        
         },
       }
     );
 
-    res.json({ message: 'Username updated successfully' });
+    res.json({ message: "Username updated successfully" });
   } catch (error) {
     next(error);
   }
@@ -118,13 +115,12 @@ const updateUserName = async (req, res, next) => {
 
 const updateUserDetails = async (req, res, next) => {
   try {
-    const email = req.query.email || '';
+    const email = req.query.email || "";
     const userData = req.body;
-
 
     if (!email) {
       return res.status(400).json({
-        message: 'Bad Request',
+        message: "Bad Request",
       });
     }
 
@@ -134,7 +130,7 @@ const updateUserDetails = async (req, res, next) => {
 
     if (!userDetails) {
       return res.status(400).json({
-        message: 'Bad request',
+        message: "Bad request",
       });
     }
 
@@ -148,7 +144,7 @@ const updateUserDetails = async (req, res, next) => {
           $set: {
             name: userData?.name,
             email: userData?.email,
-            password: userDetails?.password,    
+            password: userDetails?.password,
           },
         }
       );
@@ -157,32 +153,44 @@ const updateUserDetails = async (req, res, next) => {
         { assignedTo: email },
         { assignedTo: userData?.email }
       );
-      res.json({ message: 'Email updated successfully', updated: true });
+      res.json({ message: "Email updated successfully", updated: true });
     }
-
-    const passwordMatch = await bcrypt.compare(
-      userData?.oldPassword,
-      userDetails?.password
-    );
-
-    if (!passwordMatch) {
-      
-      return res.status(401).json({ message: 'Password did not match', passwordMatch: false });
-    } else {
-      const hashedPassword = await bcrypt.hash(userData?.newPassword, 10); 
-
-      await User.updateOne(
-        { email: email },
-        {
-          $set: {
-            name: userData?.name,
-            email: userData?.email,
-            password: hashedPassword,
-          },
-        }
+    if (
+      userData?.oldPassword.length !== 0 &&
+      userData?.newPassword.length !== 0
+    ) {
+      const passwordMatch = await bcrypt.compare(
+        userData?.oldPassword,
+        userDetails?.password
       );
 
-      res.json({ message: 'User details updated successfully', updated: true });
+      if (!passwordMatch) {
+        return res
+          .status(401)
+          .json({ message: "Password did not match", passwordMatch: false });
+      } else {
+        const hashedPassword = await bcrypt.hash(userData?.newPassword, 10);
+
+        await User.updateOne(
+          { email: email },
+          {
+            $set: {
+              name: userData?.name,
+              email: userData?.email,
+              password: hashedPassword,
+            },
+          }
+        );
+        await Task.updateMany({ user: email }, { user: userData?.email });
+        await Task.updateMany(
+          { assignedTo: email },
+          { assignedTo: userData?.email }
+        );
+        res.json({
+          message: "User details updated successfully",
+          updated: true,
+        });
+      }
     }
   } catch (error) {
     next(error);
